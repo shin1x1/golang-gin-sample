c850dd3ba2ce1f8b20762939545db1c30ee70bd3
shin1x1 <shin1x1@gmail.com>
2014-07-18 22:05:41 +0900

    init

--- httpd.go
+++ httpd.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+  "os"
+  "github.com/gin-gonic/gin"
+)
 
 func main() {
   r := gin.Default()
@@ -8,6 +11,5 @@ func main() {
     c.String(200, "pong")
   })
 
-  // Listen and server on 0.0.0.0:8080
-  r.Run(":8080")
+  r.Run(":" + os.Getenv("PORT"))
 }
